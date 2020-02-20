f33d64c3574e08bda0f8c51de8577ca02e28347e
Kirill <kirillzhelt@gmail.com>
2020-02-20 11:27:42 +0300

    Implement endpoints returning paginated json information

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,29 +1,33 @@
-from flask import jsonify
+from flask import jsonify, request
 
 from app import db
 from app.api import bp
-from app.models import Game, Favourite
+from app.models import Game, Favourite, User
 
 
 @bp.route('/')
 def index():
-    print(Game.query.first().favourites)
     return 'Hello World'
 
 
 @bp.route('/games')
 def get_games():
-    games = [{'id': 5, 'name': 'FIFA 20'}]
-    return jsonify(games)
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = Game.to_collection_dict(Game.query, page, per_page, 'api.get_games')
+    return jsonify(data)
 
 
 @bp.route('/games/<int:id>')
 def get_game(id):
-    game = {'id': id, 'name': 'FIFA 20'}
-    return jsonify(game)
+    return jsonify(Game.query.get_or_404(id).to_dict())
 
 
 @bp.route('/favs/<int:user_id>')
 def get_fav_games(user_id):
-    games = [{'id': 5, 'name': 'FIFA 20'}, {'id': 25, 'name': 'GTA V'}]
-    return jsonify(games)
+    user = User.query.get_or_404(user_id)
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = Favourite.to_collection_dict(Favourite.query.filter_by(user=user), \
+        page, per_page, 'api.get_fav_games', user_id=user_id)
+    return jsonify(data)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,32 @@
+from flask import url_for
 from sqlalchemy.schema import Table
 
 from app import db
+
+
+class PaginatedAPIMixin:
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page, per_page, False)
+        data = {
+            'items': [item.to_dict() for item in resources.items],
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total,
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page,
+                                **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page,
+                                **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page - 1, per_page=per_page,
+                                **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
+
 
 db.reflect()
 
@@ -21,12 +47,39 @@ class Screenshot(db.Model):
     __tablename__ = 'gamecatalog_screenshot'
 
 
-class Game(db.Model):
+class Game(PaginatedAPIMixin, db.Model):
     __tablename__ = 'gamecatalog_game'
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'cover_url': self.cover_url,
+            'summary': self.summary,
+            'release_data': self.release_date,
+            'rating': self.rating,
+            'rating_count': self.rating_count,
+            'aggregated_rating': self.aggregated_rating,
+            'aggregated_rating_count': self.aggregated_rating_count,
+            'screenshots': [screenshot.image_url for screenshot in self.screenshots],
+            'platforms': [platform.name for platform in self.platforms],
+            'genres': [genre.slug for genre in self.genres],
+            'keywords': [keyword.slug for keyword in self.keywords],
+        }
 
-class Favourite(db.Model):
+
+class User(db.Model):
+    __tablename__ = 'gamecatalog_user'
+
+
+class Favourite(PaginatedAPIMixin, db.Model):
     __tablename__ = 'gamecatalog_favourite'
+
+    def to_dict(self):
+        return {
+            'game': self.game.to_dict(),
+            'is_deleted': self.is_deleted,
+        }
 
 
 game_platforms_association = db.Model.metadata.tables['gamecatalog_game_platforms']
@@ -40,3 +93,4 @@ Game.keywords = db.relationship('Keyword', secondary=game_keywords_association, 
 Game.screenshots = db.relationship('Screenshot', backref='game', lazy='dynamic')
 
 Favourite.game = db.relationship('Game', backref='favourites')
+Favourite.user = db.relationship('User', backref='favourites')
